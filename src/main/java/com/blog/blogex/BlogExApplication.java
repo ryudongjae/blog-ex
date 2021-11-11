1307f5a5bdc38ce71b5f5dc5e95978d91be42ee6
ryudongjae <rdj10149@gmail.com>
2021-11-11 21:40:58 +0900

    Initial commit

---
+++ src/main/java/com/blog/blogex/BlogExApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blogex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/blogex/BlogExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blogex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
