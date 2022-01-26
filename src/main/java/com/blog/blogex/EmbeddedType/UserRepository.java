62b2ad86072c8d1adc943a4ae55e853377d336ef
ryudongjae <rdj10149@gmail.com>
2022-01-26 21:40:15 +0900

    update

--- src/main/java/com/blog/blogex/EmbeddedType/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.blog.blogex.EmbeddedType;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-}
